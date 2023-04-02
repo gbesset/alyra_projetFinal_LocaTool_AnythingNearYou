2e04d520657463768cdc71fe1513a0a0193ede5a
gbesset <guillaume.besset.pro@gmail.com>
2023-04-02 21:46:11 +0200

    Feat: add factory test intialisation

---
+++ truffle/test/AnyNFTCollectionFactoryTest.js
@@ -0,0 +1,18 @@
+const ERC4907 = artifacts.require("./AnyNFTCollectionFactory.sol");
+const { BN, expectRevert, expectEvent } = require("@openzeppelin/test-helpers");
+const { assert } = require("chai");
+
+
+contract("AnyNFTCollectionFactory", async accounts => {
+    const owner = accounts[0];
+    const renter = accounts[1];
+    const factory = accounts[2];
+    const user = accounts[3];
+
+    describe("TODO", function () {
+        
+    });
+
+});
+
+   
