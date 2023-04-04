b8a459dfbdf73a4e662eb96a70d12c1ca70f68e5
gbesset <guillaume.besset.pro@gmail.com>
2023-04-04 23:01:50 +0200

    Feat: account with image

--- client/src/components/Authent/Authent.jsx
+++ client/src/components/Authent/Authent.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Heading, Box,  Flex, Text, Button } from '@chakra-ui/react';
+import { Heading, Box,  Flex, Text, Image } from '@chakra-ui/react';
 
 export const Authent = () => {
     return (
         <Box>
             <Heading>Connectez-vous</Heading>
-            <Text mt="2rem">Vous devez vous authentifer avec votre wallet Metamask ..</Text>
-            <Text mt="1rem">Pour cela, rien de plus simple : choisissez le réseau <strong>Mumbai testnet</strong> et selectionnez votre compte</Text>
+            <Flex mt="2rem">
+                <Box flex="1.5" display="flex">
+                   <Image height="auto" src="/assets/metamask-matic.jpeg" alt="logo" />
+                </Box>
+                <Box flex="2" p={4} alignItems="center"> 
+                    <Text mt="2rem">Vous devez vous authentifer avec votre wallet Metamask ..</Text>
+                    <Text mt="1rem">Pour cela, rien de plus simple : choisissez le réseau <strong>Mumbai testnet</strong> et selectionnez votre compte</Text>
+                </Box>
+            </Flex>
         </Box>
     );
 };
