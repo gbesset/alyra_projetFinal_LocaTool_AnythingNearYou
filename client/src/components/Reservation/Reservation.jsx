8e9871af08bfbe23b712833e32000fa9c28b5df8
gbesset <guillaume.besset.pro@gmail.com>
2023-04-07 11:43:23 +0200

    Feat: tab managment

--- client/src/components/Reservation/Reservation.jsx
+++ client/src/components/Reservation/Reservation.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { RentalDetails } from './RentalDetails';
+import { Box } from '@chakra-ui/react';
+
 
 export const Reservation = ({rental}) => {
     return (
-        <div>
+        <>
+        <Box>
             <RentalDetails rental={rental} />
-        </div>
+        </Box>
+        <Box>
+
+
+        </Box>
+            <h1>Réserver l'outil {rental.tokenID}</h1>
+            <h1>Réserver</h1>
+            <p>Titre : {rental.title}</p>
+            <p>Description : {rental.description}</p>
+            <p>Token ID : {rental.tokenID}</p>
+            <p>dayPrice : {rental.dayPrice}</p>
+            <p>caution : {rental.caution}</p>
+            <p>url : {rental.tokenImgURI}</p>
+        </>
     );
 };
--- client/src/components/Reservation/index.jsx
+++ client/src/components/Reservation/index.jsx
@@ -1,32 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import { useEth } from '../../contexts/EthContext';
 import { Heading, Box, Tabs, TabList, Tab, TabPanels, TabPanel,Flex, Link, Icon, Image, Text, Button, Stack, Divider, ButtonGroup } from '@chakra-ui/react';
 import { FaUserShield } from 'react-icons/fa';
 import { Reservation } from './Reservation';
-
+import { RentalStatus } from '../../utils/Enum';
 
 export const ReservationDashboard = ({rental}) => {
     const { state: { contract, accounts, isOwner} } = useEth();
 
     const [collectionNFT, setCollectionNFT] = useState('')
+    const [rentalStatus, setRentalStatus]=useState(0)
+    
+    const [tabIndex, setTabIndex] = useState(0)
+    const handleTabsChange = (index) => {
+        setTabIndex(index)
+      }
+    
+      let selectedTab={ color: 'white', bg: 'purple.500' };
 
+    const isTabActive = (status) => {
+        return rentalStatus==status;
+    }
    
+      useEffect( () =>{    
+          setTabIndex(parseInt(rentalStatus));
+
+    }, [accounts, contract, rentalStatus]);
 
     return (
         <Box>
           {rental && (
-          
-          <>
-            <Tabs mt="3rem" variant='enclosed' colorScheme='purple' isFitted>
+              <>
+              Status : {rentalStatus}
+            <Tabs mt="3rem" variant='enclosed' colorScheme='purple' isFitted index={tabIndex} onChange={handleTabsChange}>
             <TabList>
-                <Tab _selected={{ color: 'white', bg: 'white.500' }}>Réserver</Tab>
-                <Tab> <Icon as={FaUserShield} w={5} h={5} color="white.500" mr="1rem" /> Valider</Tab>
-                <Tab isDisabled>Confirmer</Tab>
-                <Tab>Restituer</Tab>
-                <Tab> <Icon as={FaUserShield} w={5} h={5} color="white.500" mr="1rem" /> Valider retour</Tab>
-                <Tab>Confirmer retour</Tab>
+                {/* _selected={isTabActive(RentalStatus.AVAILABLE)?selectedTab:undefined} */}
+                <Tab isDisabled={!isTabActive(RentalStatus.AVAILABLE)}>Réserver</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.RENTAL_REQUESTED)}> <Icon as={FaUserShield} w={5} h={5} color="white.500" mr="1rem" /> Valider</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.RENTAL_ACCEPTED_NFT_SENT)}>Confirmer NFT</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.VALIDATE_RECEIPT_PAYMENT)}>Location</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.VALIDATE_RECEIPT_PAYMENT)}>Restituer</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.COMPLETED_USER)}> <Icon as={FaUserShield} w={5} h={5} color="white.500" mr="1rem" /> Valider retour</Tab>
+                <Tab isDisabled={!isTabActive(RentalStatus.RETURN_ACCEPTED_BY_OWNER) && !isTabActive(RentalStatus.DISPUTE) }>Confirmer retour</Tab>
             </TabList>
             <TabPanels>
                 <TabPanel>
@@ -47,19 +64,6 @@ export const ReservationDashboard = ({rental}) => {
                 </TabPanel>
             </TabPanels>
             </Tabs>
-
-
-            <h1>Réserver l'outil {rental.tokenID}</h1>
-            <div>
-            <h1>Réserver</h1>
-            <p>Titre : {rental.title}</p>
-            <p>Description : {rental.description}</p>
-            <p>Token ID : {rental.tokenID}</p>
-            <p>dayPrice : {rental.dayPrice}</p>
-            <p>caution : {rental.caution}</p>
-            <p>url : {rental.tokenImgURI}</p>
-            
-            </div>
       </>
 
 
